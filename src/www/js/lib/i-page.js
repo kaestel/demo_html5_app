3da125c551b6b3b99a709865772f810b114cc216
kaestel <martin@hvadhedderde.com>
2013-06-08 02:07:02 +0200

    added base

--- src/www/js/lib/i-page.js
+++ src/www/js/lib/i-page.js
@@ -52,8 +52,7 @@ Util.Objects["page"] = new function() {
 			u.a.setOpacity(this.cN, 1);
 
 		}
-
-		u.preloader(page, ["/img/VM-forside-image.jpg", "/img/VM-forside-typo.png", "/img/VMC_Content.jpg", "/img/VMC_page3.jpg", "/img/VMC_page4.jpg", "/img/VMC_Overlay.jpg"]);
+//		u.preloader(page, ["/img/VM-forside-image.jpg", "/img/VM-forside-typo.png", "/img/VMC_Content.jpg", "/img/VMC_page3.jpg", "/img/VMC_page4.jpg", "/img/VMC_Overlay.jpg"]);
 
 		page.cN = u.qs("#content");
 		page.cN.page = page;
@@ -86,71 +85,69 @@ Util.Objects["page"] = new function() {
 			this.transitioned = null;
 			u.a.transition(this, "none");
 
-			var text = u.qs(".text", this.page.scene_front);
-			u.a.transition(text, "all 0.3s ease-out");
-			u.a.setOpacity(text, 1);
 		}
 
+		page.loaded();
 
-
-		page.scene_front = u.qs(".scene.front");
-
-
-		page.scene_page1 = u.qs(".scene.page1");
-		// u.e.drag(page.scene_page1, [0, page.offsetHeight-page.scene_page1.offsetHeight,page.scene_page1.offsetWidth, page.scene_page1.offsetHeight], {"strict":false});
-		// page.scene_page1.picked = function() {}
-		// page.scene_page1.moved = function() {}
-		// page.scene_page1.dropped = function() {}
-
-
-		page.scene_page2 = u.qs(".scene.page2");
-		u.e.drag(page.scene_page2, [0, page.offsetHeight-page.scene_page2.offsetHeight,page.scene_page2.offsetWidth, page.scene_page2.offsetHeight], {"strict":false});
-		page.scene_page2.picked = function() {}
-		page.scene_page2.moved = function() {}
-		page.scene_page2.dropped = function() {}
-
-		page.scene_page2_video = u.qs(".video", page.scene_page2);
-		u.e.click(page.scene_page2_video);
-		page.scene_page2_video.clicked = function(event) {
-			this.clicked = null;
-
-			var video = u.qs("video", this);
-			u.as(video, "display", "block");
-			// start playback
-			video.play();
-		}
-
-
-		page.scene_page3 = u.qs(".scene.page3");
-		u.e.drag(page.scene_page3, [0, page.offsetHeight-page.scene_page3.offsetHeight,page.scene_page3.offsetWidth, page.scene_page3.offsetHeight], {"strict":false});
-		page.scene_page3.picked = function() {}
-		page.scene_page3.moved = function() {}
-		page.scene_page3.dropped = function() {}
-
-		page.scene_page3_zoom = u.qs(".zoom", page.scene_page3);
-		u.e.click(page.scene_page3_zoom);
-		page.scene_page3_zoom.clicked = function() {
-
-			var full = u.ae(document.body, "div", {"class":"full"});
-			u.a.setOpacity(full, 0);
-			u.a.transition(full, "all .3s ease-out");
-			u.a.setOpacity(full, 1);
-
-
-			u.e.click(full);
-			full.clicked = function() {
-				this.transitioned = function() {
-					this.transitioned = null;
-					this.parentNode.removeChild(this);					
-				}
-				u.a.transition(full, "all 0.3s ease-out");
-				u.a.setOpacity(full, 0);
-
-			}
-		}
-		page.scene_page3_zoom.moved = function(event) {
-			u.e.resetEvents(this);
-		}
+		// 
+		// page.scene_front = u.qs(".scene.front");
+		// 
+		// 
+		// page.scene_page1 = u.qs(".scene.page1");
+		// // u.e.drag(page.scene_page1, [0, page.offsetHeight-page.scene_page1.offsetHeight,page.scene_page1.offsetWidth, page.scene_page1.offsetHeight], {"strict":false});
+		// // page.scene_page1.picked = function() {}
+		// // page.scene_page1.moved = function() {}
+		// // page.scene_page1.dropped = function() {}
+		// 
+		// 
+		// page.scene_page2 = u.qs(".scene.page2");
+		// u.e.drag(page.scene_page2, [0, page.offsetHeight-page.scene_page2.offsetHeight,page.scene_page2.offsetWidth, page.scene_page2.offsetHeight], {"strict":false});
+		// page.scene_page2.picked = function() {}
+		// page.scene_page2.moved = function() {}
+		// page.scene_page2.dropped = function() {}
+		// 
+		// page.scene_page2_video = u.qs(".video", page.scene_page2);
+		// u.e.click(page.scene_page2_video);
+		// page.scene_page2_video.clicked = function(event) {
+		// 	this.clicked = null;
+		// 
+		// 	var video = u.qs("video", this);
+		// 	u.as(video, "display", "block");
+		// 	// start playback
+		// 	video.play();
+		// }
+		// 
+		// 
+		// page.scene_page3 = u.qs(".scene.page3");
+		// u.e.drag(page.scene_page3, [0, page.offsetHeight-page.scene_page3.offsetHeight,page.scene_page3.offsetWidth, page.scene_page3.offsetHeight], {"strict":false});
+		// page.scene_page3.picked = function() {}
+		// page.scene_page3.moved = function() {}
+		// page.scene_page3.dropped = function() {}
+		// 
+		// page.scene_page3_zoom = u.qs(".zoom", page.scene_page3);
+		// u.e.click(page.scene_page3_zoom);
+		// page.scene_page3_zoom.clicked = function() {
+		// 
+		// 	var full = u.ae(document.body, "div", {"class":"full"});
+		// 	u.a.setOpacity(full, 0);
+		// 	u.a.transition(full, "all .3s ease-out");
+		// 	u.a.setOpacity(full, 1);
+		// 
+		// 
+		// 	u.e.click(full);
+		// 	full.clicked = function() {
+		// 		this.transitioned = function() {
+		// 			this.transitioned = null;
+		// 			this.parentNode.removeChild(this);					
+		// 		}
+		// 		u.a.transition(full, "all 0.3s ease-out");
+		// 		u.a.setOpacity(full, 0);
+		// 
+		// 	}
+		// }
+		// page.scene_page3_zoom.moved = function(event) {
+		// 	u.e.resetEvents(this);
+		// }
 
 	}
 }
