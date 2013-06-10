76e63dc9a1ecab74fda6e90236646b147bade888
kaestel <martin@hvadhedderde.com>
2013-06-10 11:11:02 +0200

    test

--- src/www/js/lib/seg_desktop_include.js
+++ src/www/js/lib/seg_desktop_include.js
@@ -1,6 +1,6 @@
 document.write('<script type="text/javascript" src="/js/jes/full/seg_desktop.js"></script>');
 document.write('<script type="text/javascript" src="/js/jes/src/beta-u-preloader.js"></script>');
 document.write('<script type="text/javascript" src="/js/jes/src/u-init.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/i-page.js"></script>');
+document.write('<script type="text/javascript" src="/js/lib/i-page-mobile_touch.js"></script>');
 
 document.write('<script type="text/javascript" src="/js/lib/i-desktop.js"></script>');
