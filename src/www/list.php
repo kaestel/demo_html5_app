0a1dd5b7f6cc4be1df9151a7b35417ae01168abd
Sebberman <sebastian@hvadhedderde.com>
2013-06-11 13:33:37 +0200

    beyond dropbox

--- src/www/list.php
+++ src/www/list.php
@@ -3,27 +3,27 @@
 <?php $page_title = "HTML5 Demo" ?>
 <?php include_once($_SERVER["LOCAL_PATH"]."/templates/shell.header.php") ?>
 
-<div class="scene i:list">
+<div class="scene">
 	<h1>List</h1>
 	<ul class="productlist i:productlist">
 		<li class="product" itemscope itemtype="http://schema.org/Product">
-			<h2 itemprop="name"><a href="/produkter/deodorant">Deodorant for smelly armpits</a></h2>
+			<h2 itemprop="name"><a href="/produkter/deodorant">Pointer a</a></h2>
 			<div class="description" itemprop="description">
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tellus mauris, faucibus ut fringilla at, bibendum at metus. Morbi volutpat</p>
 			</div>
 			<div class="published_at" itemprop="releaseDate">May 26th 2013</div>
 			<div class="offer" itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-				<p class="price" itemprop="price">89</p>
+				<p class="price" itemprop="price">1599</p>
 			</div>
 		</li>
 		<li class="product" itemscope itemtype="http://schema.org/Product">
-			<h2 itemprop="name"><a href="/produkter/cokementoskit">Diet Coke and Mentos Kit</a></h2>
+			<h2 itemprop="name"><a href="/produkter/cokementoskit">Nike Air Max</a></h2>
 			<div class="description" itemprop="description">
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus tellus mauris, faucibus ut fringilla at, bibendum at metus. Morbi volutpat</p>
 			</div>
 			<div class="published_at" itemprop="releaseDate">April 3rd 2013</div>
 			<div class="offer" itemprop="offers" itemscope itemtype="http://schema.org/Offer">
-				<p class="price" itemprop="price">132,-</p>
+				<p class="price" itemprop="price">2049</p>
 			</div>
 
 		</li>
