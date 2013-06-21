328cadf76952e4e267486a8d7ac88753bbdbfe3b
Sebberman <sebastian@hvadhedderde.com>
2013-06-21 11:12:03 +0200

    cart html

--- src/www/cart.php
+++ src/www/cart.php
@@ -6,7 +6,41 @@
 <div class="scene i:cart">
 
 	<h1>Cart</h1>
-
+	<ul class="items" id="cart">
+		<li>
+			<span class="title">Gram 380g Shoe</span>
+			<span class="price">1799kr</span>
+		</li>
+		<li>
+			<span class="title">Nike Air Max</span>
+			<span class="price">2049kr</span>
+		</li>
+	</ul>
+	
+	<ul id="costs" class="costs">
+		<li class="subtotal">
+			<span class="title">Subtotal</span>
+			<span class="price">3848kr</span>
+		</li>
+		<li class="delivery">
+			<span class="title">Express</span>
+			<span class="price">200kr</span>
+		</li>
+		<li class="total">
+			<span class="title">Total</span>
+			<span class="price">4048kr</span>
+		</li>
+		<li class="vat">
+			<span class="title">Vat</span>
+			<span class="price">1799kr</span>
+		</li>
+	</ul>
+	
+	<ul class="actions">
+		<li class="shop">Back to shop</li>
+		<li class="checkout">Go to Checkout</li>
+	</ul>
+	
 </div>
 
 <?php include_once($_SERVER["LOCAL_PATH"]."/templates/shell.footer.php") ?>
