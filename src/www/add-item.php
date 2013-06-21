e6d50985552aaf55ecf7ccc323c828f1ad479fd5
Sebberman <sebastian@hvadhedderde.com>
2013-06-21 11:29:39 +0200

    add-item html

--- src/www/add-item.php
+++ src/www/add-item.php
@@ -4,21 +4,28 @@
 <?php include_once($_SERVER["LOCAL_PATH"]."/templates/shell.header.php") ?>
 
 <div class="scene i:additem">
+	
+	<ul>
+		<li>
+			<form action="#" method="post">
+				<fieldset>
 
-	<form action="#" method="post">
-		<fieldset>
+					<div class="field string required">
+						<label for="data_file_id">File</label>
+						<input type="file" id="data_file_id" name="data_file" />
+					</div>
 
-			<div class="field string required">
-				<label for="data_file_id">File</label>
-				<input type="file" id="data_file_id" name="data_file" />
-			</div>
+					<ul class="actions">
+						<li class="submit"><input class="button submit" type="submit" value="Upload"></li>
+					</ul>
 
-			<ul class="actions">
-				<li class="submit"><input class="button submit" type="submit" value="Upload"></li>
-			</ul>
+				</fieldset>
+			</form>
+			
+		</li>
+	</ul>
 
-		</fieldset>
-	</form>
+
 
 </div>
 
